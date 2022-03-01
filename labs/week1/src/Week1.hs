eb2880828b11a5f131b28264eb4d61d92194be0e
KaranVani <karan.s.vani@gmail.com>
2022-03-01 10:57:58 +0000

    my solutions

--- labs/week1/src/Week1.hs
+++ labs/week1/src/Week1.hs
@@ -2,4 +2,33 @@ module Week1 where
 
 import Prelude hiding (elem, take, length, drop)
 
+test :: String 
+test = "Hello World"
+
+square :: Int -> Int -> Int
+square x y = (x * x) + (y * y)
+
+
+elem :: Eq a => a -> [a] -> Bool
+elem x [] = False 
+elem x (y:ys) = if x == y then True else elem x ys 
+
+length :: [a] -> Int 
+length [] = 0
+length (x:xs) = 1 + length xs
+
+drop :: Int -> [a] -> [a] 
+drop 0 xs = 
+
+
+
+
+
+
+
+
+
+
+
+
 
